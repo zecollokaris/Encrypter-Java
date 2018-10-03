095d6ba580858875113d3eab618708ce5d379aac
zecollokaris <collo.kariss@gmail.com>
2018-10-03 12:03:44 +0300

    added comments where loop starts

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class CaesarCipher{
 
     public static void main(String[] args) {
+//        Set in input options which user will choose inputs which will match functions set
         System.out.print("1. Encryption\n2.Decryption\nChoose(1,2):");
+
+//        Imported Scanner Which allow us to get user input using (Scanner)
         Scanner in = new Scanner(System.in);
         int choice = in.nextInt();
+
+//        Loop For ENCRYPTION & DECRYPTION OPTIONS!
+
     }
 }
